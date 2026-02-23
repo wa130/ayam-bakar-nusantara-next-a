a560f686dd20a642864dcd890f1dee04ba39508d
Nasyuwa <nasyuwa648@gmail.com>
2026-02-23 23:42:46 +0700

    sedang membuat navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,21 @@
 import "./Navbar/Navbar.css";
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-
+        <header>
       <div className="logo">
-
         <span>Ayam Bakar Nusantara</span>
       </div>
 
       <ul className="nav-menu">
-        <li></li>
+        <li><Link href='#home'>Home</Link></li>
+        <li><Link href='#menu'>Menu</Link></li>
+        <li><Link href='#tentang-kami'>Tentang Kami</Link></li>
+        <li><Link href='#kontak'>Kontak</Link></li>
       </ul>
+      </header>
     </nav>
   );
 }
