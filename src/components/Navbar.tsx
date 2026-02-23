903fea31ee30e05a978bb56f10fdd954d3262548
Nasyuwa <nasyuwa648@gmail.com>
2026-02-24 02:56:48 +0700

    menambahkan navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,32 @@
 import "./Navbar/Navbar.css";
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-        <header>
-      <div className="logo">
-        <span>Ayam Bakar Nusantara</span>
-      </div>
+      <header>
+        <div className="logo">
+          <Image src="/logo.png"
+           alt="logo"
+           width={70}
+           height={70} />
+        </div>
 
-      <ul className="nav-menu">
-        <li><Link href='#home'>Home</Link></li>
-        <li><Link href='#menu'>Menu</Link></li>
-        <li><Link href='#tentang-kami'>Tentang Kami</Link></li>
-        <li><Link href='#kontak'>Kontak</Link></li>
-      </ul>
+        <ul className="nav-menu">
+          <li>
+            <Link href="#home">Home</Link>
+          </li>
+          <li>
+            <Link href="#menu">Menu</Link>
+          </li>
+          <li>
+            <Link href="#tentang-kami">Tentang Kami</Link>
+          </li>
+          <li>
+            <Link href="#kontak">Kontak</Link>
+          </li>
+        </ul>
       </header>
     </nav>
   );
