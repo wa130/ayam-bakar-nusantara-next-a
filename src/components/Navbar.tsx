aec20c1fd4b0b74870da03e1214ca125a612cbca
Nasyuwa <nasyuwa648@gmail.com>
2026-02-23 22:35:38 +0700

    Initial commit from Create Next App

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import "./Navbar/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+
+        <span>Ayam Bakar Nusantara</span>
+      </div>
+
+      <ul className="nav-menu">
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
