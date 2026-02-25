984a30ca2184d19d43ac723cd9a8df2aea622369
Nasyuwa <nasyuwa648@gmail.com>
2026-02-25 16:45:01 +0700

    menambahkan about section

---
+++ src/components/About.tsx
@@ -0,0 +1,35 @@
+import './About/About.css'
+import Image from 'next/image'
+
+export default function About() {
+
+    return (
+      <section className="about">
+        <h1>Tentang Kami</h1>
+        <div className="container">
+          <div className="content">
+            <h2>Tentang Ayam bakar Kami</h2>
+            <p>
+              Ayam bakkar tradisional dengan bumbu nusantara yang dibakar
+              langsung di atas arang, bukan oven
+            </p>
+            <p>
+              Kami memilih ayam segar dan kualitas terbaik setiap hari dan bumbu
+              yang diracik sendiri agar rasa tetap konsisten, beraroma asap, dan
+              meresap hingga kedalam.
+            </p>
+          </div>
+
+          <div className="image">
+            <Image
+              src="/ay.jfif"
+              width={250}
+              height={250}
+              alt="Ayam Bakar Nusantara"
+            />
+          </div>
+
+        </div>
+      </section>
+    );
+}
