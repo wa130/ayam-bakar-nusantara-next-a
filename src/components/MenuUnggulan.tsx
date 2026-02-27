a7e487519a91bf55fc9592859cfd142928a88f5e
Nasyuwa <nasyuwa648@gmail.com>
2026-02-27 19:07:54 +0700

    add data menu unggulan

--- src/components/MenuUnggulan.tsx
+++ src/components/MenuUnggulan.tsx
@@ -1,10 +1,19 @@
 import './MenuUnggulan/MenuUnggulan.css'
+import { Menu } from "@/lib/Menu"
 
 export default function MenuUnggulan() {
+    const menu = {
+        name: String
+    }
 
     return (
         <section className="menu-unggulan">
             <h1>Menu Unggulan</h1>
+
+            <div className="flex">
+                <div className="grid">
+                </div>
+            </div>
         </section>
     )
 }
---
+++ src/lib/Menu.ts
@@ -0,0 +1,9 @@
+
+
+export const Menu = [
+    { name: "ayam bakar pedas", price: 23000, description: "ayam bakkar ini sangat pedas" },
+    { name: "tempe goreg", price: 50000, description: "tempe goreng yang di goreng menggunakan wajan" },
+    { name: "sawi goreng", price: 25000, description: "sawi yang digoreng menggunakan minyak goreng" },
+    { name: "terong goreng", price: 30000, description: "terong goreng dengan bahan yang berkualitas langsung dari petani" },
+    { name: "tempe bakar", price: 17000, description: "tempe yang di goreng dengan bumbu rempah rempah sehingga menciptakan rasa yang khas" }
+]
